e4b1c425eb23f20cb762c5853f71da91cbda956a
Mamoke Akaunu <132917268+Mamoke-Akaunu@users.noreply.github.com>
2026-05-18 18:52:49 +0100

    Fix typo in database existence check

--- scripts/init_database.sql
+++ scripts/init_database.sql
@@ -18,7 +18,7 @@ WARNING:
 USE master;
 
 --- Drop and  recreate the 'DataWarehouse' database
-IF EXISTS (SELECT 1 FROM sys.databases WHERE anem = 'DataWarehouse')
+IF EXISTS (SELECT 1 FROM sys.databases WHERE name = 'DataWarehouse')
 BEGIN
   ALTER DATABASE DataWarehouse SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
   DROP DATABASE DataWarehouse
